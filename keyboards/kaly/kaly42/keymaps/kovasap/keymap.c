e541cdfa52c5071c687f117723bcdfbf94b3c77d
Kovas Palunas <kovas.palunas@gmail.com>
2024-09-24 18:22:02 -0700

    kaly keymap

---
+++ keyboards/kaly/kaly42/keymaps/kovasap/keymap.c
@@ -0,0 +1,100 @@
+// Copyright 2023 Kael Soares Augusto (@Dwctor)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum kaly_layers {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
+#define LOWER OSL(_LOWER)
+#define RAISE OSL(_RAISE)
+#define ADJUST MO(_ADJUST)
+#define BASE_GUI LM(_BASE, MOD_LGUI)
+#define SFT_MIN MT(MOD_LSFT, KC_MINS)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │Tab│ Q │ W │ F │ P │ B │       │ J │ L │ U │ Y │Del│Bsp│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Esc│ A │ R │ S │ T │ G │       │ M │ N │ E │ I │ O │ ' │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Alt│ Z │ X │ C │ D │ V │       │ K │ H │ , │ . │ / │Ent│
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │Ctl├───┐           ┌───┤ _ │
+      *               └───┤Spc├───┐   ┌───┤S- ├───┘
+      *                   └───┤Low│   │Rai├───┘
+      *                       └───┘   └───┘
+      */
+    [_BASE] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_DEL,  KC_BSPC,
+        KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LALT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                                            KC_LCTL, KC_SPC, LOWER,       RAISE,  SFT_MIN, KC_UNDS
+    ),
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │Bsp│Del│ 9 │ 8 │ 7 │ ! │       │ } │ { │ = │ & │ @ │ ` │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │ $ │Gui│ 6 │ 5 │ 4 │ * │       │ ) │ ( │ : │ | │ # │ ~ │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │ ^ │ 0 │ 3 │ 2 │ 1 │ % │       │ ] │ [ │ ; │Esc│ \ │Tab│
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │   ├───┐           ┌───┤   │
+      *               └───┤   ├───┐   ┌───┤   ├───┘
+      *                   └───┤   │   │   ├───┘
+      *                       └───┘   └───┘
+      */
+    [_LOWER] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LCTL, BASE_GUI,KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            _______, _______, ADJUST,    ADJUST,  _______, _______
+    ),
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │Mac│MA0│M2 │Mup│M1 │MWU│       │CUp│PgD│Up │PgU│CDe│CBs│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │MKi│MA1│MLf│MDn│MRg│MWD│       │Hom│Lft│Dwn│Rgt│End│Ins│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │MCt│MA2│M6 │M5 │M4 │M3 │       │CDn│CLf│CBs│CRt│   │PSn│
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │   ├───┐           ┌───┤   │
+      *               └───┤   ├───┐   ┌───┤   ├───┘
+      *                   └───┤   │   │   ├───┘
+      *                       └───┘   └───┘
+      */
+    [_RAISE] = LAYOUT_split_3x6_3(
+        KC_TAB,  MS_ACL0, KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LCTL, MS_ACL1, KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, MS_ACL2, KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            KC_LCTL, KC_SPC, ADJUST,     ADJUST,  KC_LSFT, KC_ENT
+    ),
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │Tab│F12│F11│F10│F9 │ T │       │CUp│PgD│Up │PgU│CDe│CBs│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Ctl│F8 │F7 │F6 │F5 │ G │       │Hom│Lft│Dwn│Rgt│End│Ins│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Sft│F4 │F3 │F2 │F1 │ B │       │CDn│CLf│CBs│CRt│   │PSn│
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │GUI├───┐           ┌───┤Alt│
+      *               └───┤Bsp├───┐   ┌───┤Ent├───┘
+      *                   └───┤   │   │   ├───┘
+      *                       └───┘   └───┘
+      */
+    [_ADJUST] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            KC_LCTL, KC_SPC, ADJUST,     ADJUST,  KC_LSFT, KC_ENT
+    ),
+};
